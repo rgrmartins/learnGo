43bfe1d1ba01467f1fde61efb502547457622f2d
¨rgrmartins¨ <rogermartinsdesign@gmail.com>
2021-04-19 22:53:35 -0300

    Aprendendo GO - Aula Inicial

---
+++ fundamentos/primeiro/primeiro.go
@@ -0,0 +1,13 @@
+// Programas executavei estão no pacote Main
+package main
+
+/*
+	Comentário de várias linhas
+*/
+
+import "fmt"
+
+func main() {
+	fmt.Print("Teste em GO! ")
+	fmt.Print("Primeiro Programa!")
+}
